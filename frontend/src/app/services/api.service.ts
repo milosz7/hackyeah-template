925a5ae244878910c7d0c27bf2e456ce2825bec0
Michal Delikat <michal.delikat0@gmail.com>
2024-09-21 13:41:58 +0200

    Dockerfile fix

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  private apiUrl = 'http://localhost:8000/api';
+  private apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
